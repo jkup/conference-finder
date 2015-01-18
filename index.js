c73cf4ac3ce1417337822df69c716fb573b0d3ef
Jon Kuperman <jon.kuperman@gmail.com>
2015-01-17 17:25:47 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var mysql   = require('mysql');
+var app     = express();
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    database : 'conferences'
+});
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'jade');
+app.get('/', function (req, res) {
+    connection.query('SELECT * FROM conferences', function(err, conferences, fields) {
+        if (err) throw err;
+
+        res.render('index', { conferences: conferences });
+    });
+})
+
+var server = app.listen(3000, function () {
+    console.log('Example app listening at http://localhost:3000');
+})
